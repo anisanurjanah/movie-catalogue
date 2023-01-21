9c5ec0b679891e47331b4a0e578116a95d9f3a6b
Anisa Nurjanah <anisanurjanah2705@gmail.com>
2023-01-21 15:05:30 +0700

    Initialize movie catalogue automated testing implemented

---
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-view.js
@@ -0,0 +1,56 @@
+import { createMovieItemTemplate } from '../../templates/template-creator';
+
+class FavoriteMovieSearchView {
+  getTemplate() {
+    return `
+    <div class="content">
+      <input id="query" type="text">
+      <h2 class="content__heading">Your Liked Movie</h2>
+      <div id="movie-search-container">
+        <div id="movies" class="movies">
+        </div>
+      </div>
+    </div>
+  `;
+  }
+
+  runWhenUserIsSearching(callback) {
+    document.getElementById('query').addEventListener('change', (event) => {
+      callback(event.target.value);
+    });
+  }
+
+  showMovies(movies) {
+    let html;
+    if (movies.length > 0) {
+      html = movies.reduce((carry, movie) => carry.concat(createMovieItemTemplate(movie)), '');
+    } else {
+      html = this._getEmptyMovieTemplate();
+    }
+
+    document.querySelector('.movies').innerHTML = html;
+
+    document.getElementById('movie-search-container')
+      .dispatchEvent(new Event('movies:searched:updated'));
+    document.getElementById('movies').dispatchEvent(new Event('movies:updated'));
+  }
+
+  showFavoriteMovies(movies = []) {
+    let html;
+    if (movies.length) {
+      html = movies.reduce((carry, movie) => carry.concat(createMovieItemTemplate(movie)), '');
+    } else {
+      html = this._getEmptyMovieTemplate();
+    }
+
+    document.getElementById('movies').innerHTML = html;
+
+    document.getElementById('movies').dispatchEvent(new Event('movies:updated'));
+  }
+
+  _getEmptyMovieTemplate() {
+    return '<div class="movie-item__not__found">Tidak ada film untuk ditampilkan</div>';
+  }
+}
+
+export default FavoriteMovieSearchView;
